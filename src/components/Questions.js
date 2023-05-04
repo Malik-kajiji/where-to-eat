1f68ed7f8d47fe04c3b5e1d34e3f7ad57212e273
imcoco <imcoco@gmail.com>
2023-05-03 22:40:26 -0400

    set up questions page

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/questions.css';
 
 const Questions = () => {
+
+    const questions = [
+        {
+            text: 'What are you craving?',
+            type: 'checkbox',
+            options: ['Chinese', 'Indian', 'Italian', 'Middle Eastern']
+        },
+        {
+            text: 'How long are you willing to travel?',
+            type: 'radio',
+            options: ['< 5 miles', '> 5 miles', 'anywhere']
+        },
+        {
+            text: 'In what city/town would you like to eat?',
+            type: 'text'
+        }
+    ]
+
     return (
-        <div>Questions</div>
+        <>
+            <h1>we got a few questions</h1>
+            
+        </>
     )
 }
 
